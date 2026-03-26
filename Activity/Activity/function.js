cd5cc0203e71d501df86007d6750676d74515873
Sommarfagel398 <kheniethforthree3s@gmail.com>
2026-03-26 15:02:06 +0800

    Add files via upload
    stuff

---
+++ Activity/Activity/function.js
@@ -0,0 +1,78 @@
+const URL = "https://jsonplaceholder.typicode.com/users/1"
+
+//GET
+async function getData(){
+    const res= await fetch(URL,{
+        method: 'PATCH',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            
+        })
+    })
+    const data = await res.json()
+}
+
+//POST
+async function postData(){
+    const res= await fetch(URL,{
+        method: 'PATCH',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            
+        })
+    })
+    const data = await res.json()
+}
+
+
+
+//PUT
+async function putData(){
+    const res= await fetch(URL,{
+        method: 'PATCH',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            
+        })
+    })
+    const data = await res.json()
+}
+
+
+//PATCH
+async function patchData(){
+    const res = await fetch(URL,{
+        method: 'PATCH',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            'username':'NTR vs MRT'
+        })
+        
+    })
+    const data = await res.json()
+    console.log(data)
+}
+patchData();
+
+//DELETE
+
+async function delData(){
+    const res = await fetch(URL,{
+        method: 'DELETE',
+        headers:{
+            'Content-Type':'application/json'
+        }
+    })
+    const data = await res.json();
+    console.log("Delete Status", res.status);
+    console.log("Response Data:",data);
+}
+
